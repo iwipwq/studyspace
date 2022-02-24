d921fcae3cc67f9519f11e2b169fe70afb48bdff
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-02-24 23:58:44 +0900

    deepfreeze Object properties
    deepfreeze Object properties

---
+++ JavaScript/deepFrozenPizza.js
@@ -0,0 +1,29 @@
+const pizza = {
+    name: 'Hawaiian pizza',
+    ingredients: {
+        cheese: 'mozzarella',
+        sauce: ['tomato', 'bbq'],
+        topping: {
+            fruit: 'pineapple',
+            vegetables: 'tomato',
+            meat: 'bacon'
+        }
+    }
+}
+
+function deepFreeze(target) {
+    if (target && typeof target === 'object' && !Object.isFrozen(target)) {
+        Object.freeze(target);
+        Object.keys(target).forEach(key => deepFreeze(target[key]));
+    }
+    return target;
+}
+
+deepFreeze(pizza);
+
+console.log(Object.isFrozen(pizza));
+console.log(Object.isFrozen(pizza.topping));
+console.log(Object.isFrozen(pizza.sauce));
+
+pizza.topping.meat = 'chicken';
+console.log(pizza);
