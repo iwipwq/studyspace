7dbfd3eea819a7fc7b3abb7af3440999619ddd01
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-02-26 23:06:47 +0900

    callback1
    callback1

---
+++ JavaScript/callbackfunc1.js
@@ -0,0 +1,17 @@
+function repeat(n,f) {
+    for (var i = 0; i < n; i++) {
+        f(i)
+    }
+}
+
+var logAll = function (i) {
+    console.log(i);
+}
+
+repeat(5, logAll);
+
+var logOdds = function (i) {
+    if (i % 2) console.log(i);
+};
+
+repeat(5,logOdds);
