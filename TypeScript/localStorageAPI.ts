02668a85cb896a88406e18a79759c0f38beeb469
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-05-12 22:12:03 +0900

    simulate browser local storage api
    using polymorphism, generic, interface, class

---
+++ TypeScript/localStorageAPI.ts
@@ -0,0 +1,29 @@
+interface MyStorage<T> {
+    [key:string]:T
+}
+
+class LocalStorage<T> {
+    private storage : MyStorage<T> = {}
+    set(key:string, value:T) {
+        this.storage[key] = value;
+    }
+    remove(key:string) {
+        delete this.storage[key]
+    }
+    get(key:string):T {
+        return this.storage[key]
+    }
+    clear() {
+        this.storage = {}
+    }
+}
+
+const stringsStorage = new LocalStorage<string>()
+
+stringsStorage.get("key")
+stringsStorage.set("keyname", "stringValue")
+
+const booleansStorage = new LocalStorage<boolean>()
+
+booleansStorage.get("abc")
+booleansStorage.set("boolkey", true)
