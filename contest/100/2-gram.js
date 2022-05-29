f8809b634b0bf3f56739a6e7dec2c789e678d2bb
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-05-30 01:12:14 +0900

    answer
    answer

--- contest/100/2-gram.js
+++ contest/100/2-gram.js
@@ -1,13 +1,16 @@
 function solve(string) {
-    if(string.length <= 1) return console.log(string,'Please enter only two or more character strings');
-    const splitedStringArr = string.split('')
-    const newStringArr = [];
-    splitedStringArr.forEach((char)=> {
-        newStringArr.push(char,char)
-    })
-    newStringArr.pop()
-    newStringArr.shift()
-    for(let i=0;i<newStringArr.length-1;i+=2) {
-        console.log(`${newStringArr[i]} ${newStringArr[i+1]}`)
+    // if(string.length <= 1) return console.log(string,'Please enter only two or more character strings');
+    // const splitedStringArr = string.split('')
+    // const newStringArr = [];
+    // splitedStringArr.forEach((char)=> {
+    //     newStringArr.push(char,char)
+    // })
+    // newStringArr.pop()
+    // newStringArr.shift()
+    // for(let i=0;i<newStringArr.length-1;i+=2) {
+    //     console.log(`${newStringArr[i]} ${newStringArr[i+1]}`)
+    // }
+    for(let i=0;i<string.length-1;i++) {
+        console.log(`${string[i]} ${string[i+1]}`)
     }
 }
