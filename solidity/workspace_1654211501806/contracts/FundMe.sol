84d21252484381b32f1df17caabb47cd7fff5993
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-06-07 23:27:16 +0900

    update FundMe
    reduce gas with constant, immutable,
    special function receive, fallback

--- solidity/workspace_1654211501806/contracts/FundMe.sol
+++ solidity/workspace_1654211501806/contracts/FundMe.sol
@@ -8,26 +8,28 @@ pragma solidity ^0.8.8;
 // import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
     // uint256 public number;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor (){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
         // 달러로 최소 금액을 설정하려 합니다.
         // 1. 어떻게 이 계약으로 ETH를 보낼까요?
         // number = 5;
-        require(msg.value.getConversionRate() >= minimumUsd, unicode"최소 펀딩금액에 미달합니다.");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, unicode"최소 펀딩금액에 미달합니다.");
         // revert 될 경우 이후 액션에서 소모된 가스는 반환됩니다.
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
@@ -78,7 +80,15 @@ contract FundMe {
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner, unicode"펀딩 소유자만 인출할 수 있습니다.");
+        require(msg.sender == i_owner, unicode"펀딩 소유자만 인출할 수 있습니다.");
         _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
