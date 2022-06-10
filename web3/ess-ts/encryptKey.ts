2a371072f5d4710f117cdbd4bd187b6995b06344
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-06-10 23:25:52 +0900

    Switching to TypeScript
    testnet deploy

---
+++ web3/ess-ts/encryptKey.ts
@@ -0,0 +1,27 @@
+// const ethers = require("ethers");
+// const fs = require("fs-extra");
+// require("dotenv").config();
+
+import {ethers} from "ethers";
+import fs from "fs-extra";
+import "dotenv/config";
+
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD!,
+    process.env.PRIVATE_KEY!
+  );
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json",encryptedJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+  
