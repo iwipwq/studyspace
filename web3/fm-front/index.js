96ae4eb9c9e3e2b104d089db262959bda2d3fa0b
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-06-24 23:28:30 +0900

    provider.once
    provider.once

--- web3/fm-front/index.js
+++ web3/fm-front/index.js
@@ -25,8 +25,8 @@ async function connect() {
 
 // 기부하기
 async function fund() {
-    const ethAmount = "77"
-  console.log(`${ethAmount} 만큼 기부함`);
+    const ethAmount = "1"
+  console.log(`${ethAmount} 만큼 기부할거에요`);
   if (typeof window.ethereum !== "undefined") {
     // provider / 블록체인에 연결
     // signer / wallet / someone with some gas
@@ -41,10 +41,28 @@ async function fund() {
         const transactionResponse = await contract.fund({
           value: ethers.utils.parseEther(ethAmount),
         });
+        // 트랜잭션이 채굴될때 listen
+
+        //ex) 이 tx가 끝날때까지 기다리렴
+        // await을 쓰게 되면 여기서 이 함수가 끝날때까지 코드실행을 멈추라는 뜻
+        await listenForTransactionMine(transactionResponse,provider)
+        console.log("완료!");
     } catch(error) {
         console.log(error);
     }
   }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`);
+    // 블록체인을 위한 리스너를 만들기 위해 프로미스 반환
+    // 이제 여기에서 블록이 채굴되면 어떤 일을 할건지 결정
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(`Completed with ${transactionReceipt.confirmations} confirmations`);
+            resolve()
+        })
+    })
 }
 
 // 출금하기
