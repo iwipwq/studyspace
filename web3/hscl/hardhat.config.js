8d1832f407566b68750b31649f3ebc6c52d6f739
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-07-05 23:51:31 +0900

    unit test -2
    unit test -2

--- web3/hscl/hardhat.config.js
+++ web3/hscl/hardhat.config.js
@@ -45,6 +45,6 @@ module.exports = {
     noColors: true,
     currency: "USD",
     coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "ETH"
+    token: "MATIC"
   }
 };
--- web3/hscl/test/unit/Raffle.test.js
+++ web3/hscl/test/unit/Raffle.test.js
@@ -1,27 +1,58 @@
-const { assert } = require("chai");
-const { getNamedAccounts, deployments, ethers } = require("hardhat");
-const { developmentChains } = require("../../helper-hardhat-config");
+const { assert, expect } = require("chai");
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../../helper-hardhat-config");
 
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle", async function () {
-      let raffle, vrfCoordinatorV2Mock;
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
+      const chainId = network.config.chainId;
 
       beforeEach(async function () {
-        const { deployer } = await getNamedAccounts();
+        deployer = (await getNamedAccounts()).deployer;
         await deployments.fixture(["all"]);
         raffle = await ethers.getContract("Raffle", deployer);
         vrfCoordinatorV2Mock = await ethers.getContract(
           "VRFCoordinatorV2Mock",
           deployer
         );
+        raffleEntranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
       });
 
       describe("constructor", async function () {
         it("Raffle을 정상적으로 초기화", async function () {
-            // Ideally we make our tests have just 1 assert per "it"
-            const raffleState = await raffle.getRaffleState();
-            assert.equal(raffleState.toString(), "0");
-        })
+          // Ideally we make our tests have just 1 assert per "it"
+          const raffleState = await raffle.getRaffleState();
+          assert.equal(raffleState.toString(), "0");
+          assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
+        });
+      });
+
+      describe("enterRaffle", async function () {
+        it("참가비가 충분하지 않을때 revert 시킴", async function () {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            "Raffle__NotEnoughETHEntered"
+          );
+        });
+        it("플레이어들이 참가했을때 기록함", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const playerFromContract = await raffle.getPlayer(0);
+          assert.equal(playerFromContract, deployer);
+        });
+        it("참가했을때 emit event", async function () {
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.emit(raffle, "RaffleEnter");
+        });
+        it("Raffle이 'CALCULATING'상태일때 참가를 불허함", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime",[interval.toNumber() + 1])
+          await network.provider.send("evm_mine",[]);
+          
+        });
       });
     });
