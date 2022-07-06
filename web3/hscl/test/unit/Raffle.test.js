5fc298c7a5431cccd0e884e0d86218f499d99040
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-07-06 23:51:23 +0900

    raffle unit test - 3
    raffle unit test - 3

--- web3/hscl/test/unit/Raffle.test.js
+++ web3/hscl/test/unit/Raffle.test.js
@@ -7,7 +7,7 @@ const {
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Raffle", async function () {
+  : describe("Raffle", function () {
       let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
       const chainId = network.config.chainId;
 
@@ -23,7 +23,7 @@ const {
         interval = await raffle.getInterval();
       });
 
-      describe("constructor", async function () {
+      describe("constructor", function () {
         it("Raffle을 정상적으로 초기화", async function () {
           // Ideally we make our tests have just 1 assert per "it"
           const raffleState = await raffle.getRaffleState();
@@ -32,7 +32,7 @@ const {
         });
       });
 
-      describe("enterRaffle", async function () {
+      describe("enterRaffle", function () {
         it("참가비가 충분하지 않을때 revert 시킴", async function () {
           await expect(raffle.enterRaffle()).to.be.revertedWith(
             "Raffle__NotEnoughETHEntered"
@@ -50,12 +50,92 @@ const {
         });
         it("Raffle이 'CALCULATING'상태일때 참가를 불허함", async function () {
           await raffle.enterRaffle({ value: raffleEntranceFee });
-          await network.provider.send("evm_increaseTime",[interval.toNumber() + 1])
-          await network.provider.send("evm_mine",[]);
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
           // await network.provider.request({method:"evm_mine", params:[]}); //위와 동일
           // chainlink keeper로 위장
           await raffle.performUpkeep([]);
-          await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.be.revertedWith("Raffle__NotOpen");
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.be.revertedWith("Raffle__NotOpen");
         });
       });
+
+      describe("checkUpkeep", function () {
+        it("참여자가 ETH를 지불하지 않았다면 false를 반환", async function () {
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert(!upkeepNeeded);
+        });
+        it("Raffle이 OPEN 상태가 아닐때 false를 반환", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          await raffle.performUpkeep("0x"); // ([])과 동일 '빈 바이트'임을 하드햇이 인식
+          const raffleState = await raffle.getRaffleState();
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert.equal(raffleState.toString(), "1");
+          assert.equal(upkeepNeeded, false);
+        });
+        it("충분한 시간(interval)이 지나지 않았다면 false 반환", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() - 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert(!upkeepNeeded);
+        });
+        it("1.충분한 시간(interval)이 지나고, 2.참가자가 존재하고, 3.eth가 계약 자금에 존재하고, 4.OPEN 상태일때 -> true를 반환", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.request({ method: "evm_mine", params: [] });
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert(upkeepNeeded);
+        });
+      });
+
+      describe("perfromUpkeep", function() {
+        it("checkUpkeep이 true일때만 작동", async function() {
+          await raffle.enterRaffle({value:raffleEntranceFee});
+          await network.provider.send("evm_increaseTime",[interval.toNumber() + 1]);
+          await network.provider.send("evm_mine", []);
+          const tx = await raffle.performUpkeep([]);
+          console.log(tx);
+          assert(tx);
+        })
+        it("checkUpkeep이 false를 반환했을때 revert", async function() {
+          await expect(raffle.performUpkeep([])).to.be.revertedWith("Raffle__UpkeepNotNeeded");
+        })
+        it("raffleState, emits, event 를 업데이트 하고 vrfCooridnator를 호출", async function() {
+          await raffle.enterRaffle({value:raffleEntranceFee});
+          await network.provider.send("evm_increaseTime",[interval.toNumber() + 1]);
+          await network.provider.send("evm_mine", []);
+          const txResponse = await raffle.performUpkeep([]);
+          const txReceipt = await txResponse.wait(1);
+          const requestId = txReceipt.events[1].args.requestId;
+          const raffleState = await raffle.getRaffleState();
+          assert(requestId.toNumber() > 0);
+          assert(raffleState.toString() == "1");
+        })
+      })
+
+      describe("fulfillRandomWords", function() {
+        beforeEach(async function() {
+          await raffle.enterRaffle({value:raffleEntranceFee});
+          await network.provider.send("evm_increaseTime",[interval.toNumber() + 1]);
+          await network.provider.send("evm_mine",[]);
+          
+        })
+      })
+
     });
