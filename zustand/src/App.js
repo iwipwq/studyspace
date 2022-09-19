320c5dc57f46583c2d1edea39e74f5f6cd71ec13
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-09-19 23:48:37 +0900

    subscribe
    subscribe

--- zustand/src/App.js
+++ zustand/src/App.js
@@ -2,18 +2,46 @@ import create from 'zustand';
 import logo from './logo.svg';
 import './App.css';
 
-const useStore = create((set) => ({
+const useCounterStore = create((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1})),
+  dec: () => set((state) => ({count: state.count - 1})),
+  reset: () => set({count: 0})
 }))
 
+const useDogStore = create(() => ({paw: true, snout: true, fur: true}))
+
 function Counter() {
-  const { count, inc } = useStore()
+  const { count, inc, dec, reset } = useCounterStore()
   return(
     <div className='counter'>
       <p>카운터</p>
       <span>{count}</span>
       <button onClick={inc}>+1</button>
+      <button onClick={dec}>-1</button>
+      <button onClick={reset}>리셋</button>
+    </div>
+  )
+}
+
+function Dogo() {
+  const paw1 = useDogStore.getState().paw
+  const unsub1 = useDogStore.subscribe(console.log);
+  useDogStore.setState({paw: false})
+
+  unsub1()
+
+  const destoryer = function () {
+    console.log("des")
+    useDogStore.destroy()
+    console.log("paw?",useDogStore.getState().paw);
+  } 
+
+  const paw = useDogStore((state) =>state.paw)
+  return (
+    <div>
+      <p>{paw.toString()}</p>
+      <button onClick={destoryer}>destory and listen</button>
     </div>
   )
 }
@@ -21,24 +49,14 @@ function Counter() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zustand
         </p>
         <div className='container'>
         <Counter />
-      </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+        <Dogo />
+        </div>
     </div>
   );
 }
